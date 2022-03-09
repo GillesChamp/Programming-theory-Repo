1d294aaf22f40369891a85bf77e89bce4a0e5fab
Gilles <gilles.champollion@gmail.com>
2022-03-09 17:16:43 +0100

    Get the  user name
    - Added new title scene
    - Added input user name
    - moved the Game Object to the start scene

--- Programming Theory Project/Assets/Scripts/GameManager.cs
+++ Programming Theory Project/Assets/Scripts/GameManager.cs
@@ -1,9 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
+
 
 public class GameManager : TheGame
 {
+    public TextMeshProUGUI userName;
+    public static string persistentUserName;
+
     void Start()
     {
        TheGame.Multiplier = 120f;
@@ -14,4 +20,13 @@ public class GameManager : TheGame
     {
         
     }
+
+    public void setUserName()
+    {
+        // it is called by the username gui once the username will be inserted
+        persistentUserName = userName.text;
+        print("The user name is: " + persistentUserName);
+        SceneManager.LoadScene(1);
+    }
+
 }
